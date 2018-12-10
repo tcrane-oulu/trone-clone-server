2fe8ac81208f502d012e3afa7e59c35aa345b524
tcrane-oulu <thomas.crane@student.oulu.fi>
2018-12-10 18:56:28 +0200

    Add error handler to packetio

--- src/lib/models/client.ts
+++ src/lib/models/client.ts
@@ -1,12 +1,17 @@
 import { PacketIO } from '../packets/packetio';
 import { createObjectId } from '../util/uuid';
 import { Socket } from 'net';
+import println from '../log/log';
 
 export class Client {
   readonly io: PacketIO;
   readonly id: number;
   constructor(socket: Socket) {
     this.io = new PacketIO(socket);
+    this.io.once('error', (err) => {
+      println('Socket', err.message);
+      this.io.destroy(err);
+    });
     this.id = createObjectId();
   }
 }
